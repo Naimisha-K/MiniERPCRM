f70446a9bfcaa72a3a5c58cf2e710ad04eda87d6
K Naimisha <naimisha0705@gmail.com>
2026-07-24 22:22:21 +0530

    Update API URL for production

--- frontend-react/src/services/api.ts
+++ frontend-react/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://mini-erp-backend-sfux.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
